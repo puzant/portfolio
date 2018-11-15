3c1f3592fd804e624abceb3e99edbd806ba6e4f5
puzant <PBAKJEJIAN@hotmail.com>
2018-11-15 13:29:46 +0200

    first push

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict"
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      $("#return-to-top").fadeIn(200);
+    } else {
+      $("#return-to-top").fadeOut(200);
+    }
+  });
+  $("#return-to-top").click(function() {
+    // When arrow is clicked
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      500
+    );
+  });
+
+
+  $(".anchorLink").click(function(e){
+    e.preventDefault();
+
+    var this_offset = $(this).offset();
+    var that_id     = $(this).attr("href");
+    var that_offset = $(that_id).offset();
+    var offset_diff = Math.abs(that_offset.top - this_offset.top);
+
+    var base_speed  = 100; // Time in ms per 1,000 pixels
+    var speed       = (offset_diff * base_speed) / 400;
+
+    $("html,body").animate({
+      scrollTop: that_offset.top
+    }, speed);
+  });
+
+  $('.btn-profile').click(function(e) {
+    e.preventDefault();  //stop the browser from following
+    window.location.href = '_Resume.pdf';
+});

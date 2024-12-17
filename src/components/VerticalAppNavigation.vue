a40090244fbe7a003bce708adfe1274544223dd5
Puzant <pbakjejian@hotmail.com>
2024-12-17 23:14:46 +0400

    add new vertical navigation component

---
+++ src/components/VerticalAppNavigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="verticalNavigation" id="verticalNavigation">
+    
+    <a v-for="nav in navigation" :href="nav.href" :key="nav.name">
+      <div class="tooltip">
+        <img :src="viewedSection === nav.href ? nav.activeIcon : nav.icon" alt="navigation icon">
+        <div class="tooltip-text">{{ nav.name }}</div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+  const navigation = [
+    {href: '#about', name: 'Home', icon: require('@/assets/icons/home-icon.svg'), activeIcon: require('@/assets/icons/home-active-icon.svg')},
+    {href: '#projects', name: 'Projects', icon: require('@/assets/icons/project-icon.svg'), activeIcon: require('@/assets/icons/project-active-icon.svg')},
+    {href: '#skills', name: 'Skills', icon: require('@/assets/icons/skill-icon.svg'), activeIcon: require('@/assets/icons/skill-active-icon.svg')},
+    {href: '#stories', name: 'Publications', icon: require('@/assets/icons/write-icon.svg'), activeIcon: require('@/assets/icons/write-active-icon.svg')},
+  ]
+
+  defineProps({ viewedSection: String })
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/variables';
+
+.verticalNavigation {
+  background-color: $secondaryBackgroundColor;
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 230px;
+  border-top-left-radius: 14px;
+  border-bottom-left-radius: 14px;
+
+  margin: 30px auto;
+  position: fixed;
+  right: 0;
+  top: 40px;
+  opacity: 0;
+
+  @media (max-width: 768px) {
+    display: none;    
+  }
+
+  .tooltip {
+    @include tooltip(true)
+  }
+}
+</style>

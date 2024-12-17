51c254289273887ea82e58963a89a1271763e3d0
Puzant <pbakjejian@hotmail.com>
2024-12-17 10:44:17 +0400

    add animation with intersection observer

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
   <div class="mainContainer">
     <AppNavigation />
     <div class="sidebarItemsContainer">
-      <SideBar />
+      <SideBar ref="sidebarRef" />
       
       <div class="mainSections">
-        <AboutMe />
-        <RecentProjects />
-        <TechSkills />
-        <MyPublications />
-        <ContactForm />
+        <AboutMe ref="aboutMeRef" />
+        <RecentProjects ref="projectsRef" />
+        <TechSkills ref="skillsRef" />
+        <MyPublications ref="publicationsRef" />
+        <ContactForm ref="contactFormRef" />
       </div>
     </div>
     <AppFooter />
   </div>
 </template>
 
-<script>
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+
 import AppNavigation from '@/components/AppNavigation/AppNavigation.vue';
 import SideBar from '@/components/Sidebar/Sidebar.vue';
 import ContactForm from '@/components/ContactForm/ContactForm.vue';
@@ -26,10 +28,32 @@ import AppFooter from '@/components/AppFooter/AppFooter.vue';
 import TechSkills from '@/components/TechSkills/TechSkills.vue';
 import MyPublications from '@/components/MyPublications/MyPublications.vue';
 
-export default {
-  name: 'App',
-  components: { AppNavigation, SideBar, AboutMe, ContactForm, RecentProjects, AppFooter, TechSkills, MyPublications }
-}
+const sidebarRef = ref(null)
+const projectsRef = ref(null)
+const aboutMeRef = ref(null)
+const skillsRef = ref(null)
+const publicationsRef = ref(null)
+const contactFormRef = ref(null)
+const refs = [sidebarRef, projectsRef, aboutMeRef, skillsRef, publicationsRef, contactFormRef]
+let observer = null
+
+onMounted(() => {
+  observer = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fade-in')
+      }
+    }
+  },{ threshold: 0.2 });
+
+  refs.forEach(ref => {
+    observer.observe(ref.value.$el)
+  });
+})
+
+onUnmounted(() => {
+  observer.disconnect()
+})
 </script>
 
 <style>
